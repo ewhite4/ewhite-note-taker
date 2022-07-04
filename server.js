75a5007c7af5264eb867a6858c7cc282b67cc357
ewhite <ew117mc@yahoo.com>
2022-07-04 12:46:59 -0700

    Add Heroku

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const apiRoutes = require("./Develop/Routes/api");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.json());
+app.use(express.static("public"));
+// app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('./routes/api', apiRoutes);
