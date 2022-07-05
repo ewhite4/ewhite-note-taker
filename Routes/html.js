270c3f1d64b82fb9ec701326c46b05fe0271bf17
ewhite <ew117mc@yahoo.com>
2022-07-05 11:28:38 -0700

    index.html change

--- Routes/html.js
+++ Routes/html.js
@@ -2,15 +2,15 @@ const path = require('path');
 const router = require('express').Router();
 
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './notes.html'));
+    res.sendFile(path.join(__dirname, '../../notes.html'));
 })
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'));
+    res.sendFile(path.join(__dirname, '../../index.html'));
 });
 
 router.get('*', (re, res) => {
-    res.sendFile(path.join(__dirname, "./index.html"))
+    res.sendFile(path.join(__dirname, "../../index.html"))
 });
 
 module.exports = router;
